08537ea2a96e513bac9356964895bdabb1e69a67
quatro <alfreadrhee97@gmail.com>
2026-05-30 18:51:24 +0900

    Ch08 Exercises: 01~05

---
+++ Ch08/ProgramExamples/repdigit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    bool digit_seen[10] = {false,};
+    int digit;
+    long n;
+
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+    
+    //check each digit of entered number, whether it is repeated or not
+    while (n > 0){
+        digit = n % 10;
+        if (digit_seen[digit]) break;
+        digit_seen[digit] = true;
+
+        n /= 10;
+    }
+
+    //Print the result based on digit_seen array
+    if (n > 0){
+        printf("Repeated digit.\n");
+    }
+    else{
+        printf("No repeated digit.\n");
+    }
+
+    return 0;
+}
